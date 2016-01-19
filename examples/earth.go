b17efd1b1d93f098d61a0191e4787c9d4db8eea6
Michael Fogleman <fogleman@gmail.com>
2016-01-18 21:37:50 -0500

    earth

--- examples/earth.go
+++ examples/earth.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/fogleman/ln/ln"
 	"github.com/jonas-p/go-shp"
 )
@@ -34,16 +32,7 @@ func getPaths(line *shp.PolyLine) ln.Paths {
 	return result
 }
 
-type Earth struct {
-	ln.Shape
-	Lines ln.Paths
-}
-
-func (e *Earth) Paths() ln.Paths {
-	return e.Lines
-}
-
-func LoadLines() ln.Paths {
+func LoadPaths() ln.Paths {
 	var result ln.Paths
 	file, err := shp.Open("examples/ne_10m_coastline/ne_10m_coastline.shp")
 	if err != nil {
@@ -58,24 +47,32 @@ func LoadLines() ln.Paths {
 	return result
 }
 
+type Earth struct {
+	ln.Shape
+	Lines ln.Paths
+}
+
+func (e *Earth) Paths() ln.Paths {
+	// return append(e.Lines, e.Shape.Paths()...)
+	return e.Lines
+}
+
 func Render(lines ln.Paths, matrix ln.Matrix) ln.Paths {
 	scene := ln.Scene{}
 	sphere := ln.NewSphere(ln.Vector{}, 1)
 	earth := Earth{sphere, lines}
 	shape := ln.NewTransformedShape(&earth, matrix)
 	scene.Add(shape)
-	eye := ln.Vector{3, 0, 0}
+	eye := ln.LatLngToXYZ(35.7806, -78.6389, 1).Normalize().MulScalar(3.25)
 	center := ln.Vector{}
 	up := ln.Vector{0, 0, 1}
 	return scene.Render(eye, center, up, 45, 1, 0.1, 100, 0.01)
 }
 
 func main() {
-	lines := LoadLines()
-	for i := 0; i < 360; i += 2 {
-		fmt.Println(i)
-		m := ln.Rotate(ln.Vector{0, 0, 1}, ln.Radians(float64(-i)))
-		path := fmt.Sprintf("earth%03d.png", i)
-		Render(lines, m).Render(path, 256)
-	}
+	lines := LoadPaths()
+	m := ln.Identity()
+	paths := Render(lines, m)
+	paths.Render("earth.png", 1024)
+	paths.Print()
 }
