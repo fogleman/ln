ec135fd9f55ae8a97d9c158648e64869a4ffd1d7
Michael Fogleman <fogleman@gmail.com>
2016-01-28 21:21:17 -0500

    example

--- examples/csg.go
+++ examples/csg.go
@@ -16,7 +16,7 @@ func main() {
 		ln.NewTransformedShape(ln.NewCylinder(0.4, -2, 2), ln.Rotate(ln.Vector{1, 0, 0}, ln.Radians(90))),
 		ln.NewTransformedShape(ln.NewCylinder(0.4, -2, 2), ln.Rotate(ln.Vector{0, 1, 0}, ln.Radians(90))),
 	)
-	for i := 0; i < 360; i += 2 {
+	for i := 0; i < 90; i += 2 {
 		fmt.Println(i)
 		scene := ln.Scene{}
 		m := ln.Rotate(ln.Vector{0, 0, 1}, ln.Radians(float64(i)))
@@ -24,8 +24,8 @@ func main() {
 		eye := ln.Vector{0, 6, 2}
 		center := ln.Vector{0, 0, 0}
 		up := ln.Vector{0, 0, 1}
-		width := 1024.0
-		height := 1024.0
+		width := 750.0
+		height := 750.0
 		paths := scene.Render(eye, center, up, width, height, 20, 0.1, 100, 0.01)
 		paths.WriteToPNG(fmt.Sprintf("out%03d.png", i), width, height)
 	}
