ea3d8a504bf84d3c2ed91ac92595ec86ea8bc1b1
Michael Fogleman <fogleman@gmail.com>
2016-02-10 19:29:43 -0500

    Scene.RenderWithMatrix

--- ln/scene.go
+++ ln/scene.go
@@ -39,10 +39,14 @@ func (s *Scene) Paths() Paths {
 
 func (s *Scene) Render(eye, center, up Vector, width, height, fovy, near, far, step float64) Paths {
 	aspect := width / height
-	s.Compile()
-	paths := s.Paths()
 	matrix := LookAt(eye, center, up)
 	matrix = matrix.Perspective(fovy, aspect, near, far)
+	return s.RenderWithMatrix(matrix, eye, width, height, step)
+}
+
+func (s *Scene) RenderWithMatrix(matrix Matrix, eye Vector, width, height, step float64) Paths {
+	s.Compile()
+	paths := s.Paths()
 	if step > 0 {
 		paths = paths.Chop(step)
 	}
