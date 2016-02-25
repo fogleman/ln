053ae0aa6199de2ce8e86bee94863ac0f66eb415
Pierre <pierrebai at hotmail>
2016-02-24 22:26:35 -0500

    Added new function Paths, make its Paths function pluggable, updated the example.

--- ln/function.go
+++ ln/function.go
@@ -13,10 +13,15 @@ type Function struct {
 	Function  func(x, y float64) float64
 	Box       Box
 	Direction Direction
+        PathsImpl func(f* Function) Paths
 }
 
-func NewFunction(function func(x, y float64) float64, box Box, direction Direction) Shape {
-	return &Function{function, box, direction}
+func NewFunction(function func(x, y float64) float64, box Box, direction Direction, pathsImpl func(f* Function) Paths) Shape {
+	return &Function{function, box, direction, pathsImpl}
+}
+
+func (f *Function) Paths() Paths {
+      return f.PathsImpl(f);
 }
 
 func (f *Function) Compile() {
@@ -46,7 +51,8 @@ func (f *Function) Intersect(ray Ray) Hit {
 	return NoHit
 }
 
-func (f *Function) Paths3() Paths {
+// Spiral
+func SpiralPaths(f *Function) Paths {
 	var path Path
 	n := 10000
 	for i := 0; i < n; i++ {
@@ -59,11 +65,11 @@ func (f *Function) Paths3() Paths {
 		z = math.Max(z, f.Box.Min.Z)
 		path = append(path, Vector{x, y, z})
 	}
-	// return append(f.Paths2(), path)
 	return Paths{path}
 }
 
-func (f *Function) Paths() Paths {
+// Radial
+func RadialPaths(f *Function) Paths {
 	var paths Paths
 	fine := 1.0 / 256
 	for a := 0; a < 360; a += 5 {
@@ -84,7 +90,8 @@ func (f *Function) Paths() Paths {
 	return paths
 }
 
-func (f *Function) Paths1() Paths {
+// Square grid
+func GridPaths(f *Function) Paths {
 	var paths Paths
 	step := 1.0 / 8
 	fine := 1.0 / 64
@@ -110,3 +117,58 @@ func (f *Function) Paths1() Paths {
 	}
 	return paths
 }
+
+// Wavy Spiral
+func WavySpiralPaths(f *Function) Paths {
+	var path Path
+	n := 50000
+	for i := 0; i < n; i++ {
+		t := float64(i) / float64(n)
+                angle := Radians(t*2*math.Pi*3000)
+
+                cos := math.Cos(angle)
+                sin := math.Sin(angle)
+
+                pow := math.Pow(t, 0.1)
+
+                wave_angle := angle * 100 * (1-math.Pow(t,0.99))
+                wave := math.Cos(wave_angle) * 0.1 * (1-pow)
+
+		r := 8 - pow*(8 + wave)
+
+		x := cos * r
+		y := sin * r
+
+		z := f.Function(x, y)
+		z = math.Min(z, f.Box.Max.Z)
+		z = math.Max(z, f.Box.Min.Z)
+		path = append(path, Vector{x, y, z})
+	}
+	return Paths{path}
+}
+
+func NewSpiralFunction(function func(x, y float64) float64, box Box, direction Direction) Shape {
+	return NewFunction(function, box, direction, SpiralPaths)
+}
+
+func NewRadialFunction(function func(x, y float64) float64, box Box, direction Direction) Shape {
+	return NewFunction(function, box, direction, RadialPaths)
+}
+
+func NewGridFunction(function func(x, y float64) float64, box Box, direction Direction) Shape {
+	return NewFunction(function, box, direction, GridPaths)
+}
+
+func NewWavySpiralFunction(function func(x, y float64) float64, box Box, direction Direction) Shape {
+	return NewFunction(function, box, direction, WavySpiralPaths)
+}
+
+// Double spirals
+func DoubleSpiralsPaths(f *Function) Paths {
+        return append(SpiralPaths(f), RadialPaths(f)...)
+}
+
+func NewDoubleSpiralsFunction(function func(x, y float64) float64, box Box, direction Direction) Shape {
+	return NewFunction(function, box, direction, DoubleSpiralsPaths)
+}
+
